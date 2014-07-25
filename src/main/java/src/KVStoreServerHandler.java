f97d4e0fece3fdef190f551b717b11fd5f67ecf1
abhang sonawane <abhangs@gmail.com>
2014-07-24 22:35:35 -0400

    implemented server sync on startup

--- src/main/java/src/KVStoreServerHandler.java
+++ src/main/java/src/KVStoreServerHandler.java
@@ -1,11 +1,10 @@
 package src;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import include.KeyValueStore.*;
 import org.apache.thrift.TException;
+import redis.clients.jedis.Jedis;
 
-import javax.management.monitor.StringMonitor;
-import javax.swing.text.html.HTMLDocument;
+
 import java.util.*;
 import java.util.logging.Logger;
 
@@ -15,7 +14,7 @@ public class KVStoreServerHandler implements KeyValueStore.Iface
     private long _atLeast;
     private String _serverID;
     private KVServerStatus _serverStatus;
-    private ArrayDeque<Map<String,Integer>> _backendServersQueue;
+    private HashMap<String,String> _backendServersQueue;
     private HashSet<String> _runningServerCollection;
     private HashSet<String> _deadServerCollection;
 
@@ -23,11 +22,15 @@ public class KVStoreServerHandler implements KeyValueStore.Iface
     private String _storageServerPort;
 
     private String _redisServerAddress;
-    private String _redisServerPort;
+    private int _redisServerPort;
+
+    private static Jedis _jedis;
+
+    private ArrayList<String> _serverCommandPrefixes;
 
     private static final Logger LOGGER = Logger.getLogger(KVStoreServerHandler.class.getName());
 
-    public KVStoreServerHandler(String serverID, String storageServerAddress,String storageServerPort,String redisServerAddress, String redisServerPort,ArrayDeque<Map<String, Integer>> backEndServersQueue,  long clockSeedTime)
+    public KVStoreServerHandler(String serverID, String storageServerAddress,String storageServerPort,String redisServerAddress, int redisServerPort,HashMap<String, String> backEndServersQueue,  long clockSeedTime)
     {
         this._serverID = serverID;
         this._storageServerAddress = storageServerAddress;
@@ -39,26 +42,67 @@ public class KVStoreServerHandler implements KeyValueStore.Iface
         this._backendServersQueue = backEndServersQueue;
         this._runningServerCollection = new HashSet<String>();
         this._deadServerCollection = new HashSet<String>();
+        this._jedis = new Jedis(_redisServerAddress,_redisServerPort);
+
+        _serverCommandPrefixes = new ArrayList<String>();
+        _serverCommandPrefixes.add(Utilities.getServerStatusPrefix());
+        _serverCommandPrefixes.add(Utilities.getServerAllKeysPrefix());
+
+        Initialize();
     }
 
-    private static Boolean Initialize()
+    public boolean Initialize()
     {
-        return false;
-    }
-
-    private Map<String,Integer> GetRunningServer()
-    {
-        Map<String,Integer> serverInfo = null;
-        Map<String,Integer>[] mapArray = new Map[_backendServersQueue.size()];
-        _backendServersQueue.toArray(mapArray);
-
-
-        for(Map<String,Integer> item:mapArray)
+        _serverStatus = KVServerStatus.Syncing;
+        if(!SyncServer())
         {
+           return _backendServersQueue.size()>1?false:true;
+        }
+        return true;
+    }
+
+    private KVStoreStatus SyncDataFromServer(String serverAddress, int serverPort) {
+
+        GetListResponse listResponse = RemoteGetList(serverAddress,serverPort,Utilities.getUserListPrefix());
+
+        try{
+            for(String item:listResponse.getValues())
+            {
+                String[] keyValue = item.split(":");
+                Put(keyValue[0],keyValue[1],this._serverID);
+            }
+
+            return KVStoreStatus.OK;
+
+        }catch(Exception ex)
+        {
+            return KVStoreStatus.EPUTFAILED;
+        }
+
+
+    }
+
+
+
+    private boolean SyncServer()
+    {
+        Collection<String> allServers = _backendServersQueue.values();
+
+        for(String item:allServers)
+        {
+            String[] serverInfo = item.split(":");
+            if(RemoteGet(serverInfo[0],Integer.parseInt(serverInfo[1]),Utilities.getServerStatusPrefix()).getValue().equals(KVServerStatus.Running))
+            {
+               KVStoreStatus storeStatus = SyncDataFromServer(serverInfo[0],Integer.parseInt(serverInfo[1]));
+               if(KVStoreStatus.OK==storeStatus)
+               {
+                   return true;
+               }
+            }
 
         }
 
-        return null;
+        return false;
     }
 
 
@@ -70,8 +114,18 @@ public class KVStoreServerHandler implements KeyValueStore.Iface
 
     @Override
     public GetListResponse GetList(String key) throws TException {
+        if(this._serverStatus==KVServerStatus.Running)
+        {
+            if(_serverCommandPrefixes.contains(key))
+            {
+                return ProcessCommand(key);
+            }
+        }
+
+
         return null;
     }
+
 
     @Override
     public KVStoreStatus Put(String key, String value, String clientid) throws TException {
@@ -99,6 +153,48 @@ public class KVStoreServerHandler implements KeyValueStore.Iface
     {
         return null;
     }
+
+    public synchronized GetListResponse RemoteGetList(String serverAddress, int serverPort, String key) {
+        return null;
+    }
+
+    //functions to perform various Server processes such synching, returning server status
+    private synchronized GetListResponse GetAllKeys()
+    {
+        Set<String> allKeys = _jedis.keys("*");
+        GetListResponse response = new GetListResponse();
+        ArrayList<String> keyValues = new ArrayList<String>();
+
+        try{
+
+            for(String key:allKeys)
+            {
+                String value = _jedis.get(key);
+                keyValues.add(key+":"+value);
+            }
+
+            response.setValues(keyValues);
+            response.setStatus(KVStoreStatus.OK);
+        }catch (Exception ex)
+        {
+            response.setValues(null);
+            response.setStatus(KVStoreStatus.INTERNAL_FAILURE);
+        }
+
+        return response;
+    }
+
+
+    private GetListResponse ProcessCommand(String key) {
+
+        if(key==Utilities.getServerAllKeysPrefix())
+        {
+            return GetAllKeys();
+        }
+
+       return null;
+    }
+
 }
 
 
---
+++ src/main/java/src/Utilities.java
@@ -0,0 +1,26 @@
+package src;
+
+
+import com.sun.org.glassfish.external.statistics.Statistic;
+
+import java.util.ArrayList;
+
+public class Utilities {
+
+    private static final String userListPrefix = "user_list";
+    private static final String serverStatusPrefix = "server_status";
+    private static final String serverAllKeysPrefix = "server_all_keys";
+
+    public static String getUserListPrefix() {
+        return userListPrefix;
+    }
+
+    public static String getServerStatusPrefix() {
+        return serverStatusPrefix;
+    }
+
+
+    public static String getServerAllKeysPrefix() {
+        return serverAllKeysPrefix;
+    }
+}
