5431f77aa130b426ec6cb5acf5a2a6c0a3e9acb3
abhang sonawane <abhangs@gmail.com>
2014-07-27 23:14:04 -0400

    Implemented Get,Put,GetList,RemoveFromList,AddToList

--- src/main/java/src/KVStoreServerHandler.java
+++ src/main/java/src/KVStoreServerHandler.java
@@ -2,18 +2,25 @@ package src;
 
 import include.KeyValueStore.*;
 import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
 import redis.clients.jedis.Jedis;
 
 
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
 public class KVStoreServerHandler implements KeyValueStore.Iface
 {
+    private String remoteServerRequestPrefix = "remote:";
     //variable for clock
     private long _atLeast;
     private String _serverID;
-    private static KVServerStatus _serverStatus;
+    private KVServerStatus _serverStatus;
     private HashMap<String,String> _backendServersQueue;
     private HashSet<String> _runningServerCollection;
     private HashSet<String> _deadServerCollection;
@@ -24,11 +31,15 @@ public class KVStoreServerHandler implements KeyValueStore.Iface
     private String _redisServerAddress;
     private int _redisServerPort;
 
-    private static Jedis _jedis;
+    private Jedis _jedis;
 
     private ArrayList<String> _serverCommandPrefixes;
 
     private static final Logger LOGGER = Logger.getLogger(KVStoreServerHandler.class.getName());
+
+    private ExecutorService _executorService;
+
+    private String _remoteServerRequestID;
 
     public KVStoreServerHandler(String serverID, String storageServerAddress,String storageServerPort,String redisServerAddress, int redisServerPort,HashMap<String, String> backEndServersQueue,  long clockSeedTime)
     {
@@ -44,14 +55,19 @@ public class KVStoreServerHandler implements KeyValueStore.Iface
         this._deadServerCollection = new HashSet<String>();
         this._jedis = new Jedis(_redisServerAddress,_redisServerPort);
 
+
         _serverCommandPrefixes = new ArrayList<String>();
         _serverCommandPrefixes.add(Utilities.getServerStatusPrefix());
         _serverCommandPrefixes.add(Utilities.getServerAllKeysPrefix());
 
         Initialize();
 
+        _remoteServerRequestID = remoteServerRequestPrefix+_serverID;
+
+        _executorService = Executors.newCachedThreadPool();
+
         //check initialize value and call the keeper joincluster with appropriate message
-        //then change server status to running
+        //then change server status to running if everything goes well  , should be a separate call?
 
     }
 
@@ -113,7 +129,34 @@ public class KVStoreServerHandler implements KeyValueStore.Iface
 
     @Override
     public GetResponse Get(String key) throws TException {
-        return null;
+
+        GetResponse response = new GetResponse();
+        try
+        {
+            String value = _jedis.get(key);
+
+            if(value.isEmpty())
+            {
+                throw new NullPointerException();
+            }
+
+            response.setValue(value);
+            response.setStatus(KVStoreStatus.OK);
+            return response;
+        }
+        catch(Exception ex)
+        {
+            if(ex.getClass()== NullPointerException.class)
+            {
+                response.setStatus(KVStoreStatus.EITEMNOTFOUND);
+                response.setValue(null);
+                return response;
+            }
+
+            response.setStatus(KVStoreStatus.EPUTFAILED);
+            response.setValue(null);
+            return  response;
+        }
 
     }
 
@@ -127,24 +170,276 @@ public class KVStoreServerHandler implements KeyValueStore.Iface
             }
         }
 
+        GetListResponse listResponse = new GetListResponse();
+        try
+        {
 
-        return null;
+            List<String> values = _jedis.lrange(key,0,-1);
+
+            if(values.isEmpty())
+            {
+                throw new NullPointerException();
+            }
+
+            listResponse.setValues(values);
+            listResponse.setStatus(KVStoreStatus.OK);
+            return listResponse;
+        }
+        catch(Exception ex)
+        {
+
+            if(ex.getClass()==NullPointerException.class)
+            {
+                listResponse.setValues(null);
+                listResponse.setStatus(KVStoreStatus.EITEMNOTFOUND);
+                return listResponse;
+            }
+
+            listResponse.setValues(null);
+            listResponse.setStatus(KVStoreStatus.EPUTFAILED);
+            return listResponse;
+        }
+
     }
 
 
     @Override
     public KVStoreStatus Put(String key, String value, String clientid) throws TException {
-        return null;
+
+        if (this._serverStatus==KVServerStatus.Running) {
+            //Remote put request, we do not need to domino this the other back-end servers
+            if(clientid.contains("remote"))
+            {
+               if(clientid.contentEquals(_remoteServerRequestID))
+               {
+                   try
+                   {
+                       if(!_jedis.set(key,value).equals("OK"))
+                       {
+                           throw new Exception();
+                       }
+                      return KVStoreStatus.OK;
+                   }
+                   catch (Exception ex)
+                   {
+                       return KVStoreStatus.EPUTFAILED;
+                   }
+               }
+            }
+
+            //Original request, need to domino this to other servers
+            if(clientid.contentEquals(_serverID))
+            {
+                final String fKey = key;
+                final String fValue = value;
+                try
+                {
+                    if(!_jedis.set(key,value).equals("OK"))
+                    {
+                        throw new Exception();
+                    }
+                    _executorService.execute(new Runnable() {
+
+                        String key = fKey;
+                        String value = fValue;
+
+                        @Override
+                        public void run() {
+                            Collection<String> allServersIDs = _backendServersQueue.keySet();
+
+                            for (String item : allServersIDs) {
+                                try {
+
+                                    String[]serverInfo = _backendServersQueue.get(item).split(":");
+                                    TSocket socket = new TSocket(serverInfo[0], Integer.parseInt(serverInfo[1]));
+                                    TTransport transport = socket;
+
+                                    TProtocol protocol = new TBinaryProtocol(transport);
+                                    KeyValueStore.Client client = new KeyValueStore.Client(protocol);
+
+                                    transport.open();
+
+                                    KVStoreStatus storeStatus = client.Put(key,value,remoteServerRequestPrefix+item );
+
+                                    transport.close();
+
+
+                                } catch (Exception ex)
+                                {
+
+                                }
+                            }
+                        }
+                    });
+                }
+                catch(Exception ex)
+                {
+                    return KVStoreStatus.EPUTFAILED;
+                }
+            }
+        }
+
+        return KVStoreStatus.EPUTFAILED;
+
     }
 
     @Override
     public KVStoreStatus AddToList(String key, String value, String clientid) throws TException {
-        return null;
+
+        if (this._serverStatus==KVServerStatus.Running) {
+            //a remote request
+            if(clientid.contains("remote"))
+            {
+               if(clientid.contentEquals(_remoteServerRequestID))
+               {
+                   try
+                   {
+                       if(!(_jedis.lpush(key,value)>0))
+                       {
+                           throw new Exception();
+                       }
+                       return KVStoreStatus.OK;
+                   }
+                   catch (Exception ex)
+                   {
+                       return KVStoreStatus.EPUTFAILED;
+                   }
+               }
+            }
+
+            if(clientid.contentEquals(_serverID))
+            {
+                try
+                {
+                    final String fKey = key;
+                    final String fValue = value;
+                    if(!(_jedis.lpush(key,value)>0))
+                    {
+                        throw new Exception();
+                    }
+
+                    _executorService.execute(new Runnable() {
+                        String key = fKey;
+                        String value = fValue;
+                        @Override
+                        public void run() {
+                            Collection<String> allServersIDs = _backendServersQueue.keySet();
+
+                            for (String item : allServersIDs) {
+                                try {
+
+                                    String[]serverInfo = _backendServersQueue.get(item).split(":");
+                                    TSocket socket = new TSocket(serverInfo[0], Integer.parseInt(serverInfo[1]));
+                                    TTransport transport = socket;
+
+                                    TProtocol protocol = new TBinaryProtocol(transport);
+                                    KeyValueStore.Client client = new KeyValueStore.Client(protocol);
+
+                                    transport.open();
+
+                                    KVStoreStatus storeStatus = client.AddToList(key, value, remoteServerRequestPrefix + item);
+
+                                    transport.close();
+
+
+                                } catch (Exception ex)
+                                {
+
+                                }
+                            }
+                        }
+                    });
+
+                    return KVStoreStatus.OK;
+                }
+                catch (Exception ex)
+                {
+                    return KVStoreStatus.EPUTFAILED;
+                }
+            }
+        }
+
+        return KVStoreStatus.EPUTFAILED;
     }
 
     @Override
     public KVStoreStatus RemoveFromList(String key, String value, String clientid) throws TException {
-        return null;
+
+        if (this._serverStatus==KVServerStatus.Running) {
+            if(clientid.contains("remote"))
+            {
+                if(clientid.contentEquals(_remoteServerRequestID))
+                {
+                    try
+                    {
+                        if(!(_jedis.lrem(key,1,value)>0))
+                        {
+                            throw new Exception();
+                        }
+
+                        return KVStoreStatus.OK;
+                    }
+                    catch(Exception ex)
+                    {
+                        return KVStoreStatus.EPUTFAILED;
+                    }
+                }
+            }
+
+            if(clientid.contentEquals(_serverID))
+            {
+                try
+                {
+                    final String fKey = key;
+                    final String fValue = value;
+                    if(!(_jedis.lrem(key,1,value)>0))
+                    {
+                        throw new Exception();
+                    }
+
+                    _executorService.execute(new Runnable() {
+                        String key = fKey;
+                        String value = fValue;
+                        @Override
+                        public void run() {
+                            Collection<String> allServersIDs = _backendServersQueue.keySet();
+
+                            for (String item : allServersIDs) {
+                                try {
+
+                                    String[]serverInfo = _backendServersQueue.get(item).split(":");
+                                    TSocket socket = new TSocket(serverInfo[0], Integer.parseInt(serverInfo[1]));
+                                    TTransport transport = socket;
+
+                                    TProtocol protocol = new TBinaryProtocol(transport);
+                                    KeyValueStore.Client client = new KeyValueStore.Client(protocol);
+
+                                    transport.open();
+
+                                    KVStoreStatus storeStatus = client.RemoveFromList(key, value, remoteServerRequestPrefix + item);
+
+                                    transport.close();
+
+
+                                } catch (Exception ex)
+                                {
+
+                                }
+                            }
+
+                        }
+                    });
+
+                    return KVStoreStatus.OK;
+                }
+                catch(Exception ex)
+                {
+                    return KVStoreStatus.EPUTFAILED;
+                }
+            }
+        }
+
+        return KVStoreStatus.EPUTFAILED;
     }
 
     @Override
@@ -158,6 +453,8 @@ public class KVStoreServerHandler implements KeyValueStore.Iface
     {
         return null;
     }
+
+
 
     public  GetListResponse RemoteGetList(String serverAddress, int serverPort, String key) {
         return null;
